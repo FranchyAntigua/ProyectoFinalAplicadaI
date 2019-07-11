98d59ede2b81c056ac819d2f8743d73b0445c8ea
FranchyAntigua <Franchy_antigua@ucne.edu.do>
2019-07-11 14:53:22 -0400

    BLL, DAL y Entidad.

---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+  public  class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public int CargoId { get; set; }
+        public string Nombre { get; set; }
+
+        public string Email { get; set; }
+        public int NivelUsuario { get; set; }
+
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+        public DateTime FechaIngreso { get; set; }
+
+        public Usuarios()
+        {
+            this.UsuarioId = 0;
+            this.CargoId = 0;
+            this.Nombre = string.Empty;
+            this.Email = string.Empty;
+            this.NivelUsuario = 0;
+            this.Usuario = string.Empty;
+            this.Clave = string.Empty;
+            this.FechaIngreso = DateTime.Now;
+        }
+    }
+}
